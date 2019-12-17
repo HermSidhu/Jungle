252afb8619426746a621258a2a1695d374609b03
HermSidhu <h-sidhu@hotmail.com>
2019-12-17 01:44:01 +0000

    login/signup and logout implemented

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,7 @@
 class AboutController < ApplicationController
 
+    before_filter :authorize
+
   def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+   # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
